babab9d46eabf2d5fd4fc7156c48c00d08e34269
Marcella Alzuguir <89037047+MarcellaAlzuguir@users.noreply.github.com>
2021-08-23 08:17:06 -0300

    Delete tempCodeRunnerFile.js

--- FaseZero/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-
-console.log ("Seganda-feira: Bom dia! Boa tarde! Boa noite!");
-console.log ("Terça-feira: Bom dia! Boa tarde! Boa noite!");
-console.log ("Quarta-feira: Bom dia! Boa tarde! Boa noite!");
-console.log ("Quinta-feira: Bom dia! Boa tarde! Boa noite!");
-console.log ("Sexta-feira: Bom dia! Boa tarde! Boa noite!");
