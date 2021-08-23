7d922d95a05ebd14dd416a94a6d7295e801efd24
Marcella Alzuguir <89037047+MarcellaAlzuguir@users.noreply.github.com>
2021-08-23 08:18:09 -0300

    Delete tempCodeRunnerFile.js

--- FaseTres/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-tenis = 5;
-menino = 3;
-apito = 2;
-
-console.log("Tênis + tênis + tênis =", ((2 * tenis) + (2 * tenis) + (2 * tenis)));
-console.log("Tênis + menino com apito + menino com apito =",((2 * tenis) + (menino + apito) + (menino + apito)));
-console.log("Menino com apito + 2 apitos + 2 apitos =", ((menino + apito) + (2 * apito) + (2 * apito)));
-console.log("Tênis + menino * 1 apito =", ((1 * sapato) + (( 1* menino) * (1 * apito)));
