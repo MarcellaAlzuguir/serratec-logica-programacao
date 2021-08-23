6ac635fe78a41c9599168135e0d4a499db387888
Marcella Alzuguir <89037047+MarcellaAlzuguir@users.noreply.github.com>
2021-08-23 08:18:55 -0300

    Delete tempCodeRunnerFile.js

--- FaseQuatro/OperadoresLogicos/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")();
-
-console.log("== Eleições 2020 ==");
-idade = prompt("  Digite sua idade: ");
-resposta = idade >= 16;
-
-console.log("  Você já pode votar? " + resposta);
